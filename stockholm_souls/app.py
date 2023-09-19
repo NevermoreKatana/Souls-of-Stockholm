6ff507311f14dd5a78a9cddf65e1c45307b3ffb2
KatanaNevermore <grenkachannel@hotmail.com>
2023-09-19 12:04:57 +0300

    add log in page

--- stockholm_souls/app.py
+++ stockholm_souls/app.py
@@ -1,13 +1,23 @@
 import os
 import dotenv
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, jsonify
 
 dotenv.load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET'])
+def log_in():
+    return render_template('/user/index.html')
+
+
+@app.route('/test', methods=['GET'])
+def l():
+    return jsonify('jopa')
