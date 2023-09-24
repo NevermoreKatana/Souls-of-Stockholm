a5e29b4c205ddd17799f1b53cfbf4c364b05895b
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-24 18:09:47 +0300

    update ДАТАЗАБАЗА

--- stockholm_souls/database/db.py
+++ stockholm_souls/database/db.py
@@ -10,7 +10,7 @@ dotenv.load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Создаем пул соединений
-connection_pool = psycopg2.pool.ThreadedConnectionPool(minconn=1, maxconn=10, dsn=DATABASE_URL)
+connection_pool = psycopg2.pool.ThreadedConnectionPool(minconn=0, maxconn=25, dsn=DATABASE_URL)
 
 checks = {
     'success' : {
