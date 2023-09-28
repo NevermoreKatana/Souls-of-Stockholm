f2362752602f1eb6fc4ca4454493ceae9298a7c4
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-28 20:51:42 +0300

    Обработчики убраны в отдельный модуль

--- stockholm_souls/app.py
+++ stockholm_souls/app.py
@@ -1,32 +1,20 @@
 import os
 import dotenv
-from stockholm_souls.database.validator import password_checker
-from stockholm_souls.database.db import (take_all_posts,
-                                         take_one_post,
-                                         take_comments,
-                                         add_comments,
-                                         add_new_post,
-                                         take_jwt,
-                                         )
-from stockholm_souls.api_handlers import api_blueprint
-from stockholm_souls.user_handlers import users_blueprint
-from flask import (Flask,
-                   render_template,
-                   request,
-                   flash,
-                   redirect,
-                   jsonify,
-                   flash,
-                   session)
-from flask_jwt_extended import (JWTManager)
+from stockholm_souls.database.db import take_all_posts
+from stockholm_souls.app_handlers.api_handlers import api_blueprint
+from stockholm_souls.app_handlers.user_handlers import users_blueprint
+from stockholm_souls.app_handlers.posts_handlers import posts_blueprint
+from flask import Flask, render_template, session
+from flask_jwt_extended import JWTManager
+
 dotenv.load_dotenv()
 
 app = Flask(__name__, static_folder='templates')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.register_blueprint(api_blueprint)
 app.register_blueprint(users_blueprint)
+app.register_blueprint(posts_blueprint)
 jwt = JWTManager(app)
-
 
 
 @app.route('/', methods=['GET'])
@@ -37,48 +25,10 @@ def index():
         return render_template('index.html', cu = current_user, posts=posts_data)
     return render_template('index.html',posts=posts_data)
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('/error/index.html')
-
-@app.route('/post/<id>', methods=['GET'])
-def show_post(id):
-    post_info = take_one_post(id)
-    comments_data = take_comments(id)
-    current_user = session.get('user')
-    if post_info:
-        return render_template('posts/post.html', post=post_info[0], cu=current_user, comments = comments_data)
-    return render_template('/error/index.html')
-
-
-@app.route('/post/<post_id>/comment', methods=['POST'])
-def add_comment(post_id):
-    content = request.form['comment']
-    user = session.get('user')
-    if user:
-        add_comments(post_id, content, user)
-        return redirect(f'/post/{post_id}')
-    flash('Войдите в аккаунт')
-    return redirect(f'/post/{post_id}')
-
-
-@app.route('/post/create', methods=['GET'])
-def create_post_form():
-    current_user = session.get('user')
-    return render_template('posts/create_post.html', cu=current_user)
-
-@app.route('/post/create/', methods=['POST'])
-def create_post():
-    current_user = session.get('user')
-    if current_user:
-        post_name = request.form['name']
-        contet = request.form['content']
-        user_id = current_user['id']
-        user_name = current_user['name']
-        add_new_post(user_id, user_name,post_name,contet)
-        return redirect('/')
-    flash('Сначала войдите в аккаунт')
-    return redirect('/post/create')
 
 
 @app.route('/docs', methods=['GET'])
---
+++ stockholm_souls/app_handlers/posts_handlers.py
@@ -0,0 +1,58 @@
+from flask import (Flask,
+                   render_template,
+                   request,
+                   flash,
+                   redirect,
+                   jsonify,
+                   flash,
+                   session, Blueprint)
+
+
+posts_blueprint = Blueprint('posts', __name__)
+
+from stockholm_souls.database.db import (take_all_posts,
+                                         take_one_post,
+                                         take_comments,
+                                         add_comments,
+                                         add_new_post,
+                                         )
+
+
+@posts_blueprint.route('/post/<id>', methods=['GET'])
+def show_post(id):
+    post_info = take_one_post(id)
+    comments_data = take_comments(id)
+    current_user = session.get('user')
+    if post_info:
+        return render_template('posts/post.html', post=post_info[0], cu=current_user, comments = comments_data)
+    return render_template('/error/index.html')
+
+
+@posts_blueprint.route('/post/<post_id>/comment', methods=['POST'])
+def add_comment(post_id):
+    content = request.form['comment']
+    user = session.get('user')
+    if user:
+        add_comments(post_id, content, user)
+        return redirect(f'/post/{post_id}')
+    flash('Войдите в аккаунт')
+    return redirect(f'/post/{post_id}')
+
+
+@posts_blueprint.route('/post/create', methods=['GET'])
+def create_post_form():
+    current_user = session.get('user')
+    return render_template('posts/create_post.html', cu=current_user)
+
+@posts_blueprint.route('/post/create/', methods=['POST'])
+def create_post():
+    current_user = session.get('user')
+    if current_user:
+        post_name = request.form['name']
+        contet = request.form['content']
+        user_id = current_user['id']
+        user_name = current_user['name']
+        add_new_post(user_id, user_name,post_name,contet)
+        return redirect('/')
+    flash('Сначала войдите в аккаунт')
+    return redirect('/post/create')
