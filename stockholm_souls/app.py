8c7c4e8a3fcb4fa4b75ff92a2e12fcba6f2441ef
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-28 20:40:29 +0300

    Обработчики пользователей вынесены в отдельный файл

--- stockholm_souls/app.py
+++ stockholm_souls/app.py
@@ -1,14 +1,7 @@
 import os
 import dotenv
 from stockholm_souls.database.validator import password_checker
-from stockholm_souls.database.db import (verification,
-                                         take_user_id,
-                                         take_user_info,
-                                         take_additional_user_info,
-                                         create_new_user,
-                                         create_session_data,
-                                         check_user,
-                                         take_all_posts,
+from stockholm_souls.database.db import (take_all_posts,
                                          take_one_post,
                                          take_comments,
                                          add_comments,
@@ -16,6 +9,7 @@ from stockholm_souls.database.db import (verification,
                                          take_jwt,
                                          )
 from stockholm_souls.api_handlers import api_blueprint
+from stockholm_souls.user_handlers import users_blueprint
 from flask import (Flask,
                    render_template,
                    request,
@@ -24,16 +18,15 @@ from flask import (Flask,
                    jsonify,
                    flash,
                    session)
-from flask_jwt_extended import (JWTManager,
-                                create_access_token,
-                                jwt_required,
-                                get_jwt_identity)
+from flask_jwt_extended import (JWTManager)
 dotenv.load_dotenv()
 
 app = Flask(__name__, static_folder='templates')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-jwt = JWTManager(app)
 app.register_blueprint(api_blueprint)
+app.register_blueprint(users_blueprint)
+jwt = JWTManager(app)
+
 
 
 @app.route('/', methods=['GET'])
@@ -43,70 +36,6 @@ def index():
     if current_user:
         return render_template('index.html', cu = current_user, posts=posts_data)
     return render_template('index.html',posts=posts_data)
-
-
-@app.route('/login', methods=['GET'])
-def login_form():
-    return render_template('/user/login.html')
-
-
-@app.route('/login/', methods=['POST'])
-def login_user():
-    name = request.form['uname']
-    passwd = request.form['passwd']
-    errors = verification(name, passwd)
-    if errors:
-        flash(errors)
-        return redirect('/login')
-    id = take_user_id(name)
-    user_data = create_session_data(id)
-    session['user'] = user_data
-    flash('Успешный вход')
-    return redirect(f'/profile/{id}')
-
-@app.route('/register', methods=['GET'])
-def reg_form():
-    return render_template('/user/register.html')
-
-
-@app.route('/register/', methods=['POST'])
-def register_user():
-    name = request.form['uname']
-    passwd = request.form['passwd']
-    confirm_passwd = request.form['confirm_passwd']
-    country = request.form['country']
-    gender = request.form['gender']
-    age = request.form['age']
-    errors = password_checker(passwd, confirm_passwd)
-    if check_user(name):
-        flash({'exist':'A user with this name exists'})
-        return redirect('/register')
-    elif errors:
-        flash(errors)
-        return redirect('/register')
-    secret = create_access_token(identity=name)
-    create_new_user(name, passwd, country,gender,age, secret)
-    id = take_user_id(name)
-    user_data = create_session_data(id)
-    session['user'] = user_data
-    return redirect(f'/profile/{id}')
-
-@app.route('/profile/<id>', methods=['GET'])
-def show_profile(id):
-    current_user = session.get('user')
-    user_info = take_user_info(id)
-    if user_info:
-        additional_info = take_additional_user_info(id)
-        return render_template('/user/profile.html', info = user_info, a_inf=additional_info, cu = current_user)
-    else:
-        return render_template('error/index.html')
-
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()
-    return redirect('/')
-
 
 @app.errorhandler(404)
 def page_not_found(error):
---
+++ stockholm_souls/user_handlers.py
@@ -0,0 +1,88 @@
+from flask import (Flask,
+                   render_template,
+                   request,
+                   flash,
+                   redirect,
+                   jsonify,
+                   flash,
+                   session, Blueprint)
+from stockholm_souls.database.validator import password_checker
+from stockholm_souls.database.db import (verification,
+                                         take_user_id,
+                                         take_user_info,
+                                         take_additional_user_info,
+                                         create_new_user,
+                                         create_session_data,
+                                         check_user,
+                                         )
+from flask_jwt_extended import (JWTManager,
+                                create_access_token,
+                                jwt_required,
+                                get_jwt_identity)
+
+
+users_blueprint = Blueprint('users', __name__)
+
+
+@users_blueprint.route('/login', methods=['GET'])
+def login_form():
+    return render_template('/user/login.html')
+
+@users_blueprint.route('/login/', methods=['POST'])
+def login_user():
+    name = request.form['uname']
+    passwd = request.form['passwd']
+    errors = verification(name, passwd)
+    if errors:
+        flash(errors)
+        return redirect('/login')
+    id = take_user_id(name)
+    user_data = create_session_data(id)
+    session['user'] = user_data
+    flash('Успешный вход')
+    return redirect(f'/profile/{id}')
+
+@users_blueprint.route('/register', methods=['GET'])
+def reg_form():
+    return render_template('/user/register.html')
+
+
+@users_blueprint.route('/register/', methods=['POST'])
+def register_user():
+    name = request.form['uname']
+    passwd = request.form['passwd']
+    confirm_passwd = request.form['confirm_passwd']
+    country = request.form['country']
+    gender = request.form['gender']
+    age = request.form['age']
+    errors = password_checker(passwd, confirm_passwd)
+    if check_user(name):
+        flash({'exist':'A user with this name exists'})
+        return redirect('/register')
+    elif errors:
+        flash(errors)
+        return redirect('/register')
+    secret = create_access_token(identity=name)
+    create_new_user(name, passwd, country,gender,age, secret)
+    id = take_user_id(name)
+    user_data = create_session_data(id)
+    session['user'] = user_data
+    return redirect(f'/profile/{id}')
+
+
+
+@users_blueprint.route('/profile/<id>', methods=['GET'])
+def show_profile(id):
+    current_user = session.get('user')
+    user_info = take_user_info(id)
+    if user_info:
+        additional_info = take_additional_user_info(id)
+        return render_template('/user/profile.html', info = user_info, a_inf=additional_info, cu = current_user)
+    else:
+        return render_template('error/index.html')
+
+
+@users_blueprint.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return redirect('/')
