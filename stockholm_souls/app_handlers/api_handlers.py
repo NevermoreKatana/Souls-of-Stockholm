34ed9f8cbd4bcce5988d0c5210d6db0e7ef2e1de
NevermoreKatana <grenkachannel@hotmail.com>
2023-09-29 17:08:00 +0300

    fix

--- stockholm_souls/app_handlers/api_handlers.py
+++ stockholm_souls/app_handlers/api_handlers.py
@@ -1,8 +1,8 @@
 from stockholm_souls.database.db_api_handlers import add_new_comment, check_valid_jwt_key, take_posts_api, take_one_post_api,create_new_post,check_valid_jwt
-from stockholm_souls.database.db_conn import (verification,
-                                              take_jwt,
-                                              take_user_info,
-                                              )
+from stockholm_souls.database.db_user_nadlers import (verification,
+                                                      take_user_info,
+                                                      )
+from stockholm_souls.database.db_api_handlers import take_jwt
 from flask import (Flask,
                    render_template,
                    request,
